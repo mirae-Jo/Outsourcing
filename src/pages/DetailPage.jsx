83ca7c995f42061aedb4b856470a57d040b1e109
YoungJ <wns4011@daum.net>
2023-12-07 20:43:15 +0900

    데이터 삭제

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,17 +1,16 @@
-import React, { useState } from 'react';
-import MountainData from '../mountain/mountainData.json';
+import React, {useState} from 'react';
 
 function DetailPage() {
-  const [mountain, setMountain] = useState(MountainData.mountains);
-  console.log(mountain);
+  // const [mountain, setMountain] = useState(MountainData.mountains);
+  // console.log(mountain);
 
   return (
     <>
-      <h1>{mountain[0].name}</h1>
-      <img src={mountain[0].imgUrl} alt='mountain' />
+      {/* <h1>{mountain[0].name}</h1>
+      <img src={mountain[0].imgUrl} alt="mountain" />
       <p> 위치 : {mountain[0].location}</p>
       <p>고도 : {mountain[0].height}m</p>
-      <p>난이도 : {mountain[0].difficulty}</p>
+      <p>난이도 : {mountain[0].difficulty}</p> */}
     </>
   );
 }
