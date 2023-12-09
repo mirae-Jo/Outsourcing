d263af96a13a15aacc1a9f3e08bad08cb6b3eea7
Kwon Bora <sweetpt88@gmail.com>
2023-12-09 23:34:11 +0900

    로그인 회원 추천 기능 test

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import {FaGithub} from 'react-icons/fa';
-import {Link, useNavigate} from 'react-router-dom';
 
 function Footer() {
-  const navigate = useNavigate();
   const handleGithubClick = () => {
     window.open('https://github.com/mirae-Jo/Outsourcing.git', '_blank');
   };
@@ -12,8 +10,20 @@ function Footer() {
     <ScFooterContainer>
       <ScTeamName>
         <ScGithubIcon onClick={handleGithubClick} />
-        <h4>미래조</h4>
+        <h4>한사랑 산악회</h4>
       </ScTeamName>
+      <ScTeamInfo>
+        <div>
+          <h3>산악회장 |</h3>
+          <p>조미래</p>
+        </div>
+        <div>
+          <h3>산악회원 |</h3>
+          <p>권보라, </p>
+          <p>권영준,</p>
+          <p>정유진</p>
+        </div>
+      </ScTeamInfo>
     </ScFooterContainer>
   );
 }
@@ -24,9 +34,11 @@ const ScFooterContainer = styled.div`
   height: 100px;
   background-color: #e8f6ef;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
+  justify-content: center;
   padding: 20px;
+  gap: 25px;
 `;
 
 const ScGithubIcon = styled(FaGithub)`
@@ -39,4 +51,14 @@ const ScTeamName = styled.div`
   flex-direction: row;
   align-items: center;
   gap: 5px;
+`;
+
+const ScTeamInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: small;
+  line-height: 1.3;
+  & div {
+    display: flex;
+  }
 `;
